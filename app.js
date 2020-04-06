b26fffeebadd8c2727b0456abe079a9521395aa4
zansy <zansy@outlook.com>
2020-04-07 00:25:55 +1000

    Complete the simple registration process

--- app.js
+++ app.js
@@ -1,69 +1,53 @@
-var createError = require('http-errors');
 var express = require('express');
+var app = express();
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var session = require('express-session');
 
-var app = express();
+global.dbHelper = require( './common/dbHelper' );
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+global.db = mongoose.connect("mongodb://127.0.0.1:27017/test");
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret:'secret',
+  cookie:{
+    maxAge:1000*60*30
+  }
+}));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+//Sets the directory where the view is stored
+app.set('views', require('path').join(__dirname, 'views'));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-
-//add from example document
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
-
-// add from project document
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.render('register');
-});
-app.listen(80);
-
-// 设定view engine变量，意为网页模板引擎
+//Access view page
 app.set( 'view engine', 'html' );
 app.engine( '.html', require( 'ejs' ).__express );
-// 设定views变量，意为视图存放的目录
-app.set('views', require('path').join(__dirname, 'views'));
-// 设定静态文件目录，比如本地文件
-app.use(express.static(require('path').join(__dirname, 'public')));
+
+//Invoke middleware usage
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer());
+
+// Set the static file directory, such as the local file
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Use middleware to deliver prompt messages
+app.use(function(req, res, next){
+  res.locals.user = req.session.user; //Save user information
+  var err = req.session.error;  //Save the resulting response information
+  res.locals.message = '';  // Save the HTML tag
+  if (err) res.locals.message = '<div class="alert alert-danger" style="margin-bottom: 20px;color:red;">' + err + '</div>';
+  next();
+});
+
+//Access to the routing
+require('./routes')(app);
+
+app.get('/', function(req, res) {
+  res.render('register');
+});
+
+app.listen(8080);
